98d1a1bd41dce23e4273b61a9df982a4b73512db
matthias <yasuakii@protonmail.com>
2023-02-18 20:11:30 +0100

    updatet footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     return(
         <div className="bg-slate-900 h-just5">
           <div className="flex justify-center items-center h-full gap-3 flex-col">
-              <div className="flex gap-5">
+              <div className="flex gap-5 pt-1">
                   <a href="https://github.com/mxtthiasss"><img src={githublogo} alt="githublogo" className="h-5 "/> </a>
                   <a href="https://matrix.to/#/@mxtthiass:matrix.org"><img src={matrixlogo} alt="matrixlogo" className="h-5 "/> </a>
               </div>

c273fb7f5ec90bf67360b58922b56747d386cc9b
matthias <yasuakii@protonmail.com>
2023-02-18 16:21:14 +0100

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@ function App() {
       <div>
 
             <div className="bg-gradient-to-r from-slate-900 to-blue-900 h-screen   text-white font-fredoka-one">
-
                      <Main />
-
-
-
                     <Footer />
-
             </div>
 
       </div>

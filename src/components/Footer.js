b8427c0b17404cbf6e519fdea2c957e815e68608
matthias <yasuakii@protonmail.com>
2023-02-19 20:03:40 +0100

    updatet footer v3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ import matrixlogo from '../img/matrix-logo-white.svg'
 export default function Footer() {
     return(
         <div className="bg-slate-900 h-just5">
-          <div className="flex justify-center items-center h-full gap-1 flex-col">
-              <div className="flex gap-5">
+          <div className="flex justify-center items-center h-full  flex-col">
+              <div className="flex gap-5 pt-2">
                   <a href="https://github.com/mxtthiasss"><img src={githublogo} alt="githublogo" className="h-5 "/> </a>
                   <a href="https://matrix.to/#/@mxtthiass:matrix.org"><img src={matrixlogo} alt="matrixlogo" className="h-5 "/> </a>
               </div>
-              <p className="text-xl">© All rights reserved </p>
+              <p className="text-xl pt-1">© All rights reserved </p>
           </div>
 
         </div>
